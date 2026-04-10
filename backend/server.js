c7e412ab24645d2c94f0825a12817e8a37f87375
Haiderj01 <haiderjhalod05l@gmail.com>
2026-04-10 20:20:39 +0530

    fix routing error

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,6 @@ const __dirname = path.dirname(__filename);
 // Serve frontend
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
